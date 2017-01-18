f7576189a56d9b1d6e1fe86d245e56c5af3f542f
fvu01 <frances.vu1@marist.edu>
2017-01-18 13:14:39 -0500

    hw0 and prj0

---
+++ Projects/Project0/Driver_prj0.java
@@ -0,0 +1,15 @@
+/**
+ * file: Driver_prj0.java
+ * author: Frances Vu
+ * course: CMPT 220
+ * assignment: Project 0
+ * due date: January 24, 2017 @ 18:30
+ * version: 1.0
+ * 
+ * This file contains the code for the "Hello World!" output for Project 0.
+ */
+public class Driver_prj0 {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+  }
+}
