5571a8674ce9d4b41f1b6defa9fdcf2b174f3334
fvu01 <frances.vu1@marist.edu>
2017-01-20 21:12:31 -0500

    prj 0 edit

--- Projects/0/Driver_prj0.java
+++ Projects/0/Driver_prj0.java
@@ -8,8 +8,16 @@
  * 
  * This file contains the code for the "Hello World!" output for Project 0.
  */
+/**
+ * Driver_prj0
+ * 
+ * This class contains the System.out.println which outputs "Hello World!" 
+ * exactly to the letter.
+ */
+
 public class Driver_prj0 {
   public static void main(String[] args) {
     System.out.println("Hello World!");
+    // This ouputs "Hello World!"
   }
 }
