14ac89ffd858481527cf744ad4f6f6cacf4e8e45
fvu01 <frances.vu1@marist.edu>
2017-02-21 22:43:42 -0500

    Spacing

--- labs/2/PhoneKeyPad.java
+++ labs/2/PhoneKeyPad.java
@@ -36,48 +36,56 @@ public class PhoneKeyPad {
       case 'A':        
       case 'B':
       case 'C':
-        number = 2;
-        break; 
+      number = 2;
+      break; 
+      
       case 'D':
       case 'E':
       case 'F':
-        number = 3;
-        break;
+      number = 3;
+      break;
+      
       case 'G':
       case 'H':
       case 'I':
-        number = 4;
-        break;
+      number = 4;
+      break;
+      
       case 'J':
       case 'K':
       case 'L':
-        number = 5;
-        break;
+      number = 5;
+      break;
+      
       case 'M':
       case 'N':
       case 'O':
-        number = 6;
-        break;
+      number = 6;
+      break;
+      
       case 'P':
       case 'Q':
       case 'R':
       case 'S':
-        number = 7;
-        break;
+      number = 7;
+      break;
+      
       case 'T':
       case 'U':
       case 'V':
-        number = 8;
-        break;
+      number = 8;
+      break;
+      
       case 'W':
       case 'X':
       case 'Y':
       case 'Z':
-        number = 9;
-        break;
+      number = 9;
+      break;
+      
       default:
-        number = 0;
-        break;
+      number = 0;
+      break;
     }
     
     if(number != 0) { // Outputs the keypad number for each letter
