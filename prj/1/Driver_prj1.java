56a2d456bcb65cb212d627ed678ea41d6724ab1e
fvu01 <frances.vu1@marist.edu>
2017-03-31 18:23:24 -0400

    P1

--- prj/1/Driver_prj1.java
+++ prj/1/Driver_prj1.java
@@ -18,13 +18,30 @@ import java.util.Scanner;
 public class Driver_prj1 {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
-    // write your code here  
+    
+    int rows = input.nextInt();
+    int columns = input.nextInt();
+    
+    System.out.println("\n");
+    
+    double[][] vFirst = new double[rows][columns];
+    double[][] vSecond = new double[rows][columns];
+    
+    for(int i=0; i<rows; i++)
+      for(int j=0; j<columns; j++) {
+        vFirst[i][j] = input.nextDouble();
+        vSecond[i][j] = input.nextDouble();
+      }  
+      
+    double[] cResult = convolveVectors(vResult); 
+    for(int i=0; i<vResult.length; i++)
+      System.out.print(vResult[i] + " ");
   }
 
   public static double[] convolveVectors(double[] vFirst, double[] vSecond) {
     double[] vResult = new double[(vFirst.length + vSecond.length) - 1];
-    // write your code here
-
+    
+    resultLength = (vFirst.length + vSecond.length) - 1;
     return vResult;  
   }
 }
